a091d4bd6a2ab0fe3db982f9480a312efd8c64c3
Patrick-052 <muiruripatrick052@gmail.com>
2023-08-08 00:58:27 +0300

    Adding class attr to a tag using js

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,9 +1,9 @@
 /* eslint-env jquery */ // => (we are using jQuery)
 
-document.ready(function () {
+$(() => {
 /* Adding a listener that adds/removes id's in a variable */
   const amenity = {};
-  $("li input[type=checkbox]").on('change', function () {
+  $('li input[type=checkbox]').on('change', function () {
     const amenityId = $(this).data('id');
     const amenityname = $(this).data('name');
     if ($(this).is(':checked')) {
@@ -14,15 +14,15 @@ document.ready(function () {
     $('.amenities h4').text(Object.values(amenity).sort().join(', '));
   });
 
-/* Check the staus of the APi*/
-   $.get({
-     url: 'http://127.0.0.1:5001/api/v1/status/',
-     success: function (data) {
-       if (data.status === 'OK') {
-         $('div#api-status').addClass('available');
-       } else {
-         $('div#api-status').removeClass('available');
-       }
-     }
-   });
+  /* Check the staus of the APi */
+  $.get({
+    url: 'http://127.0.0.1:5001/api/v1/status/',
+    success: function (data) {
+      if (data.status === 'OK') {
+        $('div#api_status').addClass('available');
+      } else {
+        $('div#api_status').removeClass('available');
+      }
+    }
+  });
 });
