60f999554a5cd46a0cac344be3b4dece34567e93
Patrick-052 <muiruripatrick052@gmail.com>
2023-08-07 11:08:15 +0300

    fixing readyness

--- web_dynamic/static/styles/1-hbnb.js
+++ web_dynamic/static/styles/1-hbnb.js
@@ -1,14 +1,16 @@
 /* eslint-env jquery */ // => (we are using jQuery)
 /* Adding a listener that adds/removes id's in a variable */
 
-const amenity = {};
-$('input[type="checkbox"]').on('change', function () {
-  const amenityId = $(this).data('id');
-  const amenityname = $(this).data('name');
-  if ($(this).is(':checked')) {
-    amenity[amenityId] = amenityname;
-  } else {
-    delete amenity[amenityId];
-  }
-  $('.amenities h4').text(Object.values(amenity).join(', '));
+$(() => {
+  const amenity = {};
+  $('input[type="checkbox"]').on('change', function () {
+    const amenityId = $(this).data('id');
+    const amenityname = $(this).data('name');
+    if ($(this).is(':checked')) {
+      amenity[amenityId] = amenityname;
+    } else {
+      delete amenity[amenityId];
+    }
+    $('.amenities h4').text(Object.values(amenity).join(', '));
+  });
 });
